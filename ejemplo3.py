e192510cbc29429bf0e182240bf1ae0d49ab7a3a
Isatam2024 <160289429+Isatam2024@users.noreply.github.com>
2024-02-23 20:59:29 -0500

    Add files via upload

---
+++ ejemplo3.py
@@ -0,0 +1,10 @@
+# se requiere conocer el area area de un reectangulo. realice un algoritmo para tal fin y representelo mediante un diagrama de flujo y el pseudocodigo para realizar este proceso.
+
+
+#se requiere determinar el sueldo semanal de un trabajador con base en las horas que trabaja y el pago por hora que recibe. Realice el diagrama de flujo y el pseudocodigo que representen el algoritmo  de solución correspondiente.
+HT = 47
+PH = 5500
+SS = HT * PH
+nombre = input("ingrese su nombre:")
+print(f"el salario semanal de {nombre}es {SS}")
+print (nombre)
