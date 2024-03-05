0144286ecf2aed940b881e332d5aaa60862698c9
Isatam2024 <160289429+Isatam2024@users.noreply.github.com>
2024-03-05 10:28:11 -0500

    punto_2.py

--- punto_2.py
+++ punto_2.py
@@ -6,9 +6,9 @@ num3 = float (input("Ingrese el num3:"))
 #mostrar mayor de tres valores
 if num1 > num2 and num1 > num3:
     print (f"el mayor de los tres números es el {num1}")
-if num2 > num1 and num2 > num3:
+elif num2 > num1 and num2 > num3:
     print(f"el mayor de los tres números es el {num2}")
-if num3 > num1 and num3 > num2:
+elif num3 > num1 and num3 > num2:
     print(f"el mayor de los números es el{num3}")
 else:
     print("Error,los número ingresados son similares.")
