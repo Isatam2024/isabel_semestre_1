3433f541fbc88aa6607135d80ccb7e12916848f4
Isatam2024 <160289429+Isatam2024@users.noreply.github.com>
2024-03-12 10:54:02 -0500

    Add files via upload

---
+++ Taller_evaluativo_operadores_punto_2.py
@@ -0,0 +1,32 @@
+#2.2.	Si a=7 y b=3, ¿Cuál es el resultado de cada una de las siguientes comparaciones?
+a = 7
+b = 3
+if a !=b :
+    print(True)
+else:
+    print(False)
+if a==b:
+    print(True)
+else:
+    print(False)
+if a > b :
+    print(True)
+else:
+    print(False)
+if (a+b) < 2 :
+    print(True)
+else:
+    print(False)
+if (a>7) and (b==3) :
+    print(True)
+else:
+    print(False)
+if  (a<7) or (b==3) :
+    print(True)
+else:
+    print(False)
+if (a<7) or (b!=3) :
+    print(True)
+else:
+    print(False)
+
